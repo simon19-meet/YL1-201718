e03f863adaeb78450c18d8a3541da5e177271dae
simon19-meet <simon19@meet.mit.edu>
2017-11-29 19:04:17 +0200

    Uploaded the code

--- Lab5/lab5.py
+++ Lab5/lab5.py
@@ -18,11 +18,54 @@ class Square(Turtle):
         
 class Rectangle(Turtle):
    def __init__(self,width,height):
-       print("hi")
+       Turtle.__init__(self)
+       self.begin_poly()
+       self.pu()
+       for i in range(2):
+           self.fd(width)
+           self.rt(90)
+           self.fd(height)
+           self.rt(90)
+       self.end_poly()
+       g = self.get_poly()
+       register_shape("rectangle",g)
+       self.shape("rectangle")
 square1=Square(5)
 square1.random_color()
+rectangle=Rectangle(10,20)
+
 #rectangle=Rectangle()
 #exersice 2
+
+class Hexagon(Turtle):
+    def __init__(self,size,speed,times):
+        Turtle.__init__(self)
+        self.shapesize(size)
+        
+        self.begin_poly()
+        self.pu()
+        for i in range(6):
+            
+            self.rt(60)
+            self.fd(100)
+        self.end_poly()
+        p = self.get_poly()
+        register_shape("hexagon",p)
+        self.shape("hexagon")
+        #def move_hx():
+        for cancer in range(times*2):
+            self.fd(times)
+        #def SetSpeed():
+        self.speed(speed)
+            
+hexagon=Hexagon(1,1,10)
+
+
+
+
+
+
+        
 
 
 
